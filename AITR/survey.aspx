6855fdce94a5c4c91ccf982fdca73b3c5474461c
Spike Ganush <florian.jourdain@gmail.com>
2022-04-17 15:18:46 +1000

    Base

---
+++ AITR/survey.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="survey.aspx.cs" Inherits="AITR.survey" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ AITR/survey.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace AITR
+{
+    public partial class survey : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection myConn;
+
+            myConn = new SqlConnection();
+
+            String targetConnection = ConfigurationManager.ConnectionStrings["CurrentConnection"].ConnectionString;
+
+            if (targetConnection.Equals("dev"))
+            {
+                myConn.ConnectionString = AppConstant.DevConnectionString;
+            }
+            else if (targetConnection.Equals("test"))
+            {
+                myConn.ConnectionString = AppConstant.TestConnectionString;
+            }
+            else if (targetConnection.Equals("prod"))
+            {
+                myConn.ConnectionString = AppConstant.ProdConnectionString;
+            }
+            else
+            {
+                throw new Exception();
+            }
+
+            myConn.Open();
+
+            SqlCommand myCommand;
+            myCommand = new SqlCommand("SELECT * FROM [question]", myConn);
+
+        }
+    }
+}
